51a10965ce42458066191c14d8fe89670d582ffc
clock-head <james.n.kai92@gmail.com>
2025-05-02 11:23:45 +1000

    cors revise

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 //Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://sunrise-cafe-ae8267.netlify.app',
     method: ['POST'],
   })
 );
